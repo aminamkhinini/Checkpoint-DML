68358d6dd7fef2228922532deecf2805536f908b
aminamkhinini <amina.mkhinini@gmail.com>
2021-06-05 17:58:01 +0200

    first commit

---
+++ DML .sql
@@ -0,0 +1,43 @@
+INSERT INTO Product
+(
+Product_id,
+Product_Name,
+Category,
+Price
+)
+VALUES
+(
+P01,P02
+Samsung Galaxy S20,Asus Notebook
+Smatphone,PC
+3299,4599
+);
+INSERT INTO Customer
+(
+Customer_id,
+Customer_Name,
+Customer_Tel,
+)
+VALUES
+(
+C01,C02,
+ALI,ASMA,
+71321009,77345823,
+);
+
+INSERT INTO Orders
+(
+Customer_id,
+Product_id,
+OrderDate,
+Quantity,
+Total_amount,
+)
+VALUES
+(
+C01,C02,
+P01,P02,
+NULL,28/05/2020,
+2,1,
+9198,3299,
+);
